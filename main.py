5bafb9732a1738b123ea5c1d17a14a080e98884e
송석리(Song Sukree) <greatsong21@gmail.com>
2025-04-16 14:33:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ for col in df.columns[1:]:
     df[col] = (df[col] * 100).round(2)
 
 # 앱 제목 (이모지 활용)
-st.header("🧑🏻‍💻 청소년 SW 동행 프로젝트와 함께하는! 👩🏻‍💻")
+st.header("🧑🏻‍💻 취미는 코딩 교학공과 함께하는! 👩🏻‍💻")
 st.title("🌍 국가별 MBTI 성향 분석 프로젝트 🔍")
 
 # 국가 선택
